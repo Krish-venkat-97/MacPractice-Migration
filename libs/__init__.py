d16ec0c3b76a13d0e93582e17aafe3d4e84bd0df
sakthi krishna <sakthi.krishna@outlook.com>
2025-12-18 10:51:44 +0000

    essential files created

---
+++ libs/__init__.py
@@ -0,0 +1,31 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from configparser import ConfigParser
+import pymysql
+import pandas as pd
+import pandasql as ps
+import dask.dataframe as dd
+from tqdm import tqdm
+from datetime import date
+import logging
+import numpy as np
+import shutil
+import sys
+import subprocess
+
+__all__ = [
+    'ConfigParser',
+    'os',
+    'pymysql',
+    'pd',
+    'ps',
+    'dd',
+    'tqdm',
+    'date',
+    'logging',
+    'np',
+    'shutil',
+    'sys',
+    'subprocess'
+]
---
+++ src/utils.py
@@ -0,0 +1,75 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from libs import *
+from src.config import config
+
+
+def get_src_myconnection():
+    try:
+        return pymysql.connect( 
+            host= config['source_mysql']['host'], 
+            user= config['source_mysql']['user'], 
+            db= config['source_mysql']['db'],
+            password= config['source_mysql']['password'])
+        print('Connection successful')
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+    
+src_connection = get_src_myconnection()
+
+def get_tgt_myconnection():
+    try:
+        return pymysql.connect( 
+            host= config['target_mysql']['host'], 
+            user= config['target_mysql']['user'], 
+            db= config['target_mysql']['db'],
+            password= config['target_mysql']['password'])
+        print('Connection successful')
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+    
+tgt_connection = get_tgt_myconnection()
+
+def safe_value(value):
+    if value is None or pd.isnull(value):
+        return 'NULL'
+    elif isinstance(value,str):
+        if '"' in value:
+            return '"' + value.replace('"',"'") + '"'
+        elif "\\" in value:
+            return '"' + value.replace("\\","\\\\") + '"'
+        else:
+            return '"' + value + '"'
+    elif isinstance(value,date):
+        return '"' + value.strftime('%Y-%m-%d') + '"'
+    else:
+        return value
+
+
+if __name__ == "__main__":
+    connection1 = get_src_myconnection()
+    if connection1:
+        print("Connection to source successful!")
+        connection1.close()
+    else:
+        print("Connection to source failed!")
+    connection2 = get_tgt_myconnection()
+    if connection2:
+        print("Connection to targetsuccessful!")
+        connection2.close()
+    else:
+        print("Connection to target failed!")
+
+def getSourceFilePath():
+    return config['source_file_path']['source_file_path']
+
+
+def getTargetFilePath():
+    return config['target_file_path']['target_file_path']
+
+def getLogFilePath():
+    return config['log_directory']['log_directory']
+
