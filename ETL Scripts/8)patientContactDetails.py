47e0b72e9ab910004a6d6f92d7db69947f4a583c
Emeka Okonji <okonjiemeka@outlook.com>
2026-01-05 13:58:08 +0000

    installed libraries

---
+++ ETL Scripts/8)patientContactDetails.py
@@ -0,0 +1,10 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from libs import *
+from src.utils import *
+
+source_myconnection = get_src_myconnection()
+
+target_myconnection = get_tgt_myconnection()
+target_cursor = target_myconnection.cursor()
